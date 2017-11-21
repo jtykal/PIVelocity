9ca812ae8095b7af4c624ddf93bcd117846cfbae
Jim Tykal <jtykal@users.noreply.github.com>
2017-11-21 16:47:05 -0600

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -3,18 +3,8 @@ Ext.define('Utils', {
 
     getFieldForAggregationType: function(aggregationType) {
         switch(aggregationType) {
-            case 'acceptedleafcount':
-                return 'AcceptedLeafStoryCount';
-            case 'acceptedleafplanest':
-                return 'AcceptedLeafStoryPlanEstimateTotal';
-            case 'leafcount':
-                return 'LeafStoryCount';
-            case 'leafplanest':
-                return 'LeafStoryPlanEstimateTotal';
-            case 'prelimest':
-                return 'PreliminaryEstimateValue';
-            case 'refinedest':
-                return 'RefinedEstimate';
+            case 'planestimate':
+                return 'PlanEstimate';
             default:
                 return null;
         }
